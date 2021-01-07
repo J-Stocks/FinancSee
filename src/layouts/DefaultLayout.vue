8ab588d606eccdd3090883baeee98d68cc036059
Joshua Stocks <joshua.stocks@hotmail.com>
2021-01-07 21:30:17 +0000

    DefaultLayout is now actually reactive

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,16 +1,18 @@
 <template>
-  <div>
-    <nav>
-      <slot name="nav"></slot>
-    </nav>
-    <main>
-      <slot name="main"></slot>
-    </main>
+  <div class="font-sans bg-gray-200">
+    <div class="container min-h-screen mx-auto p-1">
+      <nav class="flex space-x-2 text-3xl whitespace-no-wrap leading-none mb-2">
+        <slot name="nav"></slot>
+      </nav>
+      <main class="text-lg">
+        <slot name="main"></slot>
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
   export default {
-  name: "DefaultLayout"
-}
+    name: "DefaultLayout"
+  }
 </script>
