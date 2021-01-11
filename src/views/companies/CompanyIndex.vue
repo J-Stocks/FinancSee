9ead56c44bae4b072d377511f4f87616b36f85a8
Joshua Stocks <joshua.stocks@hotmail.com>
2021-01-11 00:12:46 +0000

    Added text searching to CompanyIndex currently both company name and symbol are searched.

--- src/views/companies/CompanyIndex.vue
+++ src/views/companies/CompanyIndex.vue
@@ -2,11 +2,22 @@
   <default-layout>
     <template v-slot:nav>
       <nav-link :route-to="{name: 'Home'}" link-text="FS"/>
+      <input
+          v-model="searchString"
+          type="text"
+          placeholder="Search"
+          title="Search Companies"
+          autofocus
+          maxlength="300"
+          size="10"
+          spellcheck="false"
+          class="text-xl bg-gray-200 placeholder-gray-500 border-2 border-black rounded px-1 focus:border-blue-900 hover:border-blue-900 ease-in-out"
+      />
       <button
           v-on:click="previousPage"
           title="Previous Page"
           type="button"
-          class="border-2 border-black rounded px-1 focus:border-blue-900 focus:text-blue-900 hover:border-blue-900 hover:text-blue-900 ease-in-out"
+          class="align-middle border-2 border-black rounded px-1 focus:border-blue-900 focus:text-blue-900 hover:border-blue-900 hover:text-blue-900 ease-in-out"
       >
         ←
       </button>
@@ -15,15 +26,15 @@
           v-on:click="nextPage"
           title="Next Page"
           type="button"
-          class="border-2 border-black rounded px-1 focus:border-blue-900 focus:text-blue-900 hover:border-blue-900 hover:text-blue-900 ease-in-out"
+          class="align-middle border-2 border-black rounded px-1 focus:border-blue-900 focus:text-blue-900 hover:border-blue-900 hover:text-blue-900 ease-in-out"
       >
         →
       </button>
     </template>
     <template v-slot:main>
       <div>
-        <div v-if="companies && companies.length !== 0" class="inline-grid gird-cols-2-auto gap-2">
-          <template v-for="company in companiesToShow">
+        <div v-if="visibleCompanies && visibleCompanies.length !== 0" class="inline-grid gird-cols-2-auto gap-2">
+          <template v-for="company in visibleCompanies">
             <router-link :to="{name: 'CompanyDetails', params: {symbol: company.symbol}}" :key="'symbol' + company.id">
               <strong class="whitespace-nowrap">{{ company.symbol }}</strong>
             </router-link>
@@ -32,7 +43,10 @@
             </router-link>
           </template>
         </div>
-        <p v-else class="h-full text-center text-3xl">No Companies Found</p>
+        <p v-else-if="allCompanies && allCompanies.length !== 0" class="h-full text-center text-3xl">
+          No Matching Companies Found
+        </p>
+        <p v-else class="h-full text-center text-3xl">Loading</p>
       </div>
     </template>
   </default-layout>
@@ -51,35 +65,64 @@
     },
     data: function () {
       return {
-        companies: [],
+        allCompanies: [],
         companiesPerPage: 20,
-        currentPage: 0
+        visibleCompanies: [],
+        currentPage: 0,
+        searchString: ''
       }
     },
     computed: {
-      companiesToShow() {
-        return this.companies.slice(
-            this.currentPage * this.companiesPerPage,
-            (this.currentPage * this.companiesPerPage) + this.companiesPerPage
-        );
-      },
       pageCount() {
-        return Math.ceil(this.companies.length / this.companiesPerPage);
+        return Math.ceil(this.allCompanies.length / this.companiesPerPage);
+      }
+    },
+    watch: {
+      currentPage() {
+        this.updateCompaniesToShow();
+      },
+      searchString() {
+        this.updateCompaniesToShow();
       }
     },
     created() {
-      AlphaVantage.getAllCompanies().then(results => this.companies = results);
+      AlphaVantage.getAllCompanies().then(results => {
+        this.allCompanies = results;
+        this.updateCompaniesToShow();
+      });
     },
     methods: {
       nextPage() {
-        if (this.companies[(this.currentPage + 1) * this.companiesPerPage] !== undefined) {
+        if (this.allCompanies[(this.currentPage + 1) * this.companiesPerPage] !== undefined) {
           this.currentPage++;
         }
       },
       previousPage() {
-        if (this.currentPage !== 0) {
+        if (this.currentPage > 0) {
           this.currentPage--;
         }
+      },
+      updateCompaniesToShow() {
+        let tempCompanies = this.allCompanies;
+        if (this.searchString.trim()) {
+          let searchTerms = this.searchString
+              .trim()
+              .split(/\s/)
+              .map(term => term.replace(/[^\w\d]/g, ''))
+              .filter(term => term !== '')
+          ;
+          if (searchTerms) {
+            let masterPattern = new RegExp(searchTerms.join('|'), 'i');
+            tempCompanies = tempCompanies.filter(company => {
+              return company.symbol.match(masterPattern) || company.name.match(masterPattern);
+            });
+          }
+          this.currentPage = 0;
+        }
+        this.visibleCompanies = tempCompanies.slice(
+            this.currentPage * this.companiesPerPage,
+            (this.currentPage * this.companiesPerPage) + this.companiesPerPage
+        );
       }
     }
   }
