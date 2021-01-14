798941655a8713604b3ec31156992edfaa0205a3
Joshua Stocks <joshua.stocks@hotmail.com>
2021-01-14 03:01:18 +0000

    Request to the Alpha Vantage API are now retried upon failure, with configurable numbers of attempts and delay between attempts.

--- src/alpha-vantage.js
+++ src/alpha-vantage.js
@@ -4,6 +4,10 @@ import dayjs from "dayjs";
 export default class AlphaVantage {
     static apiKey = process.env.VUE_APP_ALPHA_VANTAGE_KEY;
 
+    static apiAttempts = 5;
+
+    static apiTimeout = 5000;
+
     static baseUrl = "https://www.alphavantage.co/query?";
 
     static companiesPromise;
@@ -17,39 +21,36 @@ export default class AlphaVantage {
     //left as is.
     static getAllCompanies() {
         if (this.companiesPromise === undefined) {
-            this.companiesPromise = fetch(`${this.baseUrl}function=LISTING_STATUS&apikey=${this.apiKey}`)
-                .then(response => response.text())
-                .then(csv => {
-                    let companies = Papa.parse(csv, {header: true, skipEmptyLines: true}).data;
-                    companies.forEach((company, index) => company.id = index);
-                    return companies;
-                })
-                .catch(error => console.log('Error', error))
-            ;
+            this.companiesPromise = this.retryFetch(
+                `${this.baseUrl}function=LISTING_STATUS&apikey=${this.apiKey}`,
+                false
+            ).then(csv => {
+                let companies = Papa.parse(csv, {header: true, skipEmptyLines: true}).data;
+                companies.forEach((company, index) => company.id = index);
+                return companies;
+            }).catch(error => console.log('Error', error));
         }
         return this.companiesPromise;
     }
 
     static getAllCurrencies() {
         if (this.currenciesPromise === undefined) {
-            this.currenciesPromise = fetch('https://www.alphavantage.co/physical_currency_list/')
-                .then(response => response.text())
-                .then(csv => {
-                    let currencies = Papa.parse(csv, {header: true, skipEmptyLines: true}).data;
-                    currencies.forEach((company, index) => company.id = index);
-                    return currencies;
-                })
-                .catch(error => console.log('Error', error))
-            ;
+            this.currenciesPromise = this.retryFetch(
+                'https://www.alphavantage.co/physical_currency_list/',
+                false
+            ).then(csv => {
+                let currencies = Papa.parse(csv, {header: true, skipEmptyLines: true}).data;
+                currencies.forEach((company, index) => company.id = index);
+                return currencies;
+            }).catch(error => console.log('Error', error));
         }
         return this.currenciesPromise;
     }
 
     static getCompanyBySymbol(symbol) {
-        return fetch(`${this.baseUrl}function=OVERVIEW&symbol=${symbol}&apikey=${this.apiKey}`)
+        return this.retryFetch(`${this.baseUrl}function=OVERVIEW&symbol=${symbol}&apikey=${this.apiKey}`)
             .then(response => response.json())
-            .catch(error => console.log('Error', error))
-        ;
+            .catch(error => console.log('Error', error));
     }
 
     static getCompanyName(symbol) {
@@ -57,46 +58,86 @@ export default class AlphaVantage {
     }
 
     static getCurrencyTimeSeries(fromCode, toCode) {
-        return fetch(
+        return this.retryFetch(
             this.baseUrl +
             `function=FX_DAILY` +
             `&from_symbol=${fromCode}` +
             `&to_symbol=${toCode}` +
             `&outputsize=full` +
             `&apikey=${this.apiKey}`
-            ).then(response => response.json())
-            .then(json => {
-                let timeSeries = []
-                for (const key of Object.keys(json["Time Series FX (Daily)"])) {
-                    timeSeries[key] = Number.parseFloat(json["Time Series FX (Daily)"][key]["4. close"]);
-                }
-                return timeSeries;
-            })
-            .catch(error => console.log('Error', error))
-        ;
+        ).then(json => {
+            let timeSeries = []
+            for (const key of Object.keys(json["Time Series FX (Daily)"])) {
+                timeSeries[key] = Number.parseFloat(json["Time Series FX (Daily)"][key]["4. close"]);
+            }
+            return timeSeries;
+        }).catch(error => console.log('Error', error));
     }
 
     static getStockTimeSeries(symbol){
-        return fetch(
+        return this.retryFetch(
             this.baseUrl +
             `function=TIME_SERIES_DAILY_ADJUSTED` +
             `&symbol=${symbol}` +
             `&outputsize=full` +
             `&apikey=${this.apiKey}`
-            ).then(response => response.json())
-            .then(json => {
-                let output = {
-                    close: [],
-                    volume: []
-                };
-                for (const key of Object.keys(json["Time Series (Daily)"])) {
-                    output.close[key] = Number.parseFloat(json["Time Series (Daily)"][key]["5. adjusted close"]);
-                    output.volume[key] = Number.parseFloat(json["Time Series (Daily)"][key]["6. volume"]);
+        ).then(json => {
+            let output = {
+                close: [],
+                volume: []
+            };
+            for (const key of Object.keys(json["Time Series (Daily)"])) {
+                output.close[key] = Number.parseFloat(json["Time Series (Daily)"][key]["5. adjusted close"]);
+                output.volume[key] = Number.parseFloat(json["Time Series (Daily)"][key]["6. volume"]);
+            }
+            return output;
+        }).catch(error => console.log('Error', error));
+    }
+
+    static retryFetch(
+        url,
+        getJson = true,
+        retryAttempts = this.apiAttempts,
+        timeout = this.apiTimeout
+    ) {
+        return fetch(url).then(response => {
+            if (response.ok) {
+                if (getJson) {
+                    return response.json();
+                } else {
+                    return response.text();
                 }
-                return output;
-            })
-            .catch(error => console.log('Error', error))
-        ;
+            } else if (retryAttempts > 0) {
+                console.log(`Warning: request failed with code ${response.status},` +
+                    `retrying in ${timeout / 1000} seconds. ${retryAttempts} attempts remaining`
+                );
+                return new Promise(resolve => {
+                    setTimeout(() => {
+                        resolve(this.retryFetch(url, getJson, retryAttempts - 1, timeout))
+                    }, timeout);
+                });
+            } else {
+                throw new Error(response);
+            }
+        }).then(content => {
+            //The Alpha Vantage API does not actually return an error when exceeding the call frequency limit.
+            //Instead json data is returned matching the following data:
+            const failMessage = "{\n    \"Note\": \"Thank you for using Alpha Vantage! Our standard API call frequency is 5 calls per minute and 500 calls per day. Please visit https://www.alphavantage.co/premium/ if you would like to target a higher API call frequency.\"\n}"
+            if ((getJson && content['Note'] === undefined) || (!getJson && content !== failMessage)) {
+                return content;
+            } else if (retryAttempts > 0) {
+                console.log(`Warning: API request frequency too high, retrying in ${timeout / 1000} seconds.` +
+                    ` ${retryAttempts} attempts remaining`
+                );
+                return new Promise(resolve => {
+                    setTimeout(() => {
+                        resolve(this.retryFetch(url, getJson, retryAttempts - 1, timeout))
+                    }, timeout);
+                });
+            } else {
+                throw new Error(content['Note']);
+            }
+        }).catch(error => console.log(error));
     }
 
     static sliceTimeSeries(fullTimeSeries, fromDate, toDate) {
